2add39e720802656dc68fa974cf5344cfd1e7081
Brut <niklas.k.151@gmail.com>
2023-10-07 00:47:16 +0200

    Added functional game end screen

--- Assets/Scripts/GameEndUi.cs
+++ Assets/Scripts/GameEndUi.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameEndUi : MonoBehaviour
 {
     public TMP_Text Text;
+    public Image Bg;
 
     public void Win()
     {
         gameObject.SetActive(true);
         Text.text = "You have woken up and remembered your memory";
+        Text.color = Color.black;
+        Bg.color = Color.white;
     }
 
     public void Lose()
     {
         gameObject.SetActive(true);
         Text.text = "You have died";
+        Text.color = Color.white;
+        Bg.color = Color.black;
     }
 
     public void Restart()
@@ -27,6 +33,9 @@ public class GameEndUi : MonoBehaviour
 
     public void Exit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
         Application.Quit();
     }
 }
