aeb32014f8358a17f3f9e56eeb1795e18f18d0a4
Knrc <kontakt.mikey@gmail.com>
2023-10-07 14:52:23 +0200

    DoDotDamage - Almost works it just has to do damage and move down

--- Assets/Scripts/DoDotDamage.cs
+++ Assets/Scripts/DoDotDamage.cs
@@ -9,38 +9,54 @@ public class DoDotDamage : MonoBehaviour
     public float time;
     private bool _damage;
     private bool _timerActive;
+    public Life life;
 
-    public void DotTarget(GameObject target)
+
+    public void DotTargetTimer(GameObject target)
     {
-        if (target.TryGetCommponentInLineage<Life>(out var life))
-        {
-            if(_damage)
-            {
-                life.Damage(amount);
-                _damage = false;
-            }
-
-        }
+        Debug.Log("Enter collisison");
+        _timerActive = true;
     }
 
-    public void DamageThis()
+    public void DotTargetTimerOff(GameObject target)
     {
-        DotTarget(gameObject);
+        Debug.Log("Exit collision");
+        _timerActive = false;
+        _timeForDot = time;
+        
+    }
+
+    public void DotTargetDamage()
+    {
+        Debug.Log("Damage");
+        
+        life.Damage(amount);
+    }
+
+    void Update()
+    {
+        Timer();
     }
 
     private void Timer()
     {
+        Debug.Log("Timer is listening");
         if(_timerActive)
         {
+            Debug.Log("Timer is Active");
             if(_timeForDot > 0)
             {
                 _timeForDot -= Time.deltaTime;
             }
+            else if(_timeForDot < 0 && _damage)
+            {
+                DotTargetDamage();
+                _damage = false;
+                _timeForDot = time;
+            }
             else
             {
-                Debug.Log("Dot damage applied.");
                 _damage = true;
-                _timeForDot = time;
             }
         }
     }
