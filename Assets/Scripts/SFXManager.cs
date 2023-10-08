f202f5e500b57d2a0fdcfd0a35621c5a5c304947
Knrc <kontakt.mikey@gmail.com>
2023-10-08 15:24:37 +0200

    Renamed Files

---
+++ Assets/Scripts/SFXManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip wakeUpSFX, walkSFX, jumpSFX, damageSFX, pickUpSFX;
+    // Start is called before the first frame update
+
+    public void WakeUpSFX()
+    {
+
+    }
+    public void WalkSFX()
+    {
+        
+    }
+
+    public void JumpSFX()
+    {
+        
+    }
+
+    public void DamageSFX()
+    {
+        
+    }
+
+    public void PickupSFX()
+    {
+        
+    }
+}
